b33a7b568eba4a26089571c74c3fe9cc56ac9904
Vanessa Böhner <boehner.vanessa@gmail.com>
2018-06-24 15:20:00 +0200

    adds demo folder

---
+++ src/components_demo/CTAGhost.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    :class="{'btn-cta--disabled': !isActive}"
+    :disabled="!isActive"
+    @click.prevent="addToCart()"
+    class="btn btn--full btn-cta btn-cta--ghost"
+  >
+    Add to Cart
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CTAGhost',
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+  methods: {
+    addToCart() {
+      if (this.isActive) {
+        // add item to cart
+      }
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+}
+
+.btn-cta {
+  font-size: 16px;
+  padding: 15px 32px;
+}
+
+.btn-cta--ghost {
+  background-color: white;
+  border: 1px solid #64B587;
+  color: #64B587;
+}
+
+.btn-cta--disabled {
+  background-color: #ddd;
+}
+</style>
---
+++ src/components_demo/CTA_solution.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :class="{'btn-cta--disabled': !isActive}"
+    @click.prevent="clickHandler()"
+    class="btn btn--full btn-cta btn-cta--green"
+  >
+    Add to Cart
+  </button>
+</template>
+
+<script>
+import { add } from 'lodash';
+
+export default {
+  name: 'CTA',
+  data() {
+    return {
+      isActive: true,
+			counter: 0,
+    };
+  },
+  methods: {
+		clickHandler() {
+			if (this.isActive) {
+				this.addToCart();
+			}
+			this.counter = add(this.counter, 1);
+			this.isActive = !this.isActive;
+		},
+    addToCart() {
+			// add item to cart
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+}
+
+.btn-cta {
+  font-size: 16px;
+  padding: 15px 32px;
+}
+
+.btn-cta--green {
+  background-color: #64B587;
+  border: none;
+  color: white;
+}
+
+.btn-cta--disabled {
+  background-color: #ddd;
+}
+</style>
